f1279c56c2a97ffb5307286bd56e504e526ec911
BrendaAbigailVC <109320578+BrendaAbigailVC@users.noreply.github.com>
2025-06-06 01:09:50 -0500

    courses

--- lib/services/course_service.dart
+++ lib/services/course_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:programovilfront/models/courses.dart';
+import 'package:programovilfront/models/faculty.dart'; 
 
 class CourseService {
   Future<List<dynamic>> loadCoursesFromJsonAsMap() async {
@@ -13,5 +14,23 @@ class CourseService {
         await rootBundle.loadString('assets/json/courses.json');
     final List<dynamic> data = json.decode(response);
     return data.map((e) => Course.fromJson(e)).toList();
+  }
+
+  Future<List<Faculty>> loadFacultiesFromJson() async {
+    final String response =
+        await rootBundle.loadString('assets/json/faculties.json');
+    final List<dynamic> data = json.decode(response);
+    return data.map((e) => Faculty.fromJson(e)).toList();
+  }
+
+  Future<List<Course>> getCoursesByCollegeId(int collegeId) async {
+    final faculties = await loadFacultiesFromJson();
+    final facultyIds = faculties
+        .where((f) => f.collegeId == collegeId)
+        .map((f) => f.facultyId)
+        .toSet();
+
+    final courses = await loadCoursesFromJson();
+    return courses.where((c) => facultyIds.contains(c.facultyId)).toList();
   }
 }
