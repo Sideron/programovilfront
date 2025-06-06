3be679659b92d1df44c88c7a8530cad4f4aa6d96
Sideron <guardian2012cave@gmail.com>
2025-06-06 01:17:54 -0500

    Update course_service.dart

--- lib/services/course_service.dart
+++ lib/services/course_service.dart
@@ -1,9 +1,20 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:programovilfront/models/courses.dart';
-import 'package:programovilfront/models/faculty.dart'; 
+import 'package:programovilfront/models/faculty.dart';
 
 class CourseService {
+  Future<Course> getCourseId(int id) async {
+    final jsonStr = await rootBundle.loadString('assets/json/courses.json');
+    List<dynamic> data = json.decode(jsonStr);
+    Course myCourse = data
+        .map((json) => Course.fromJson(json))
+        .where((x) => x.courseId == id)
+        .toList()
+        .first;
+    return myCourse;
+  }
+
   Future<List<dynamic>> loadCoursesFromJsonAsMap() async {
     final jsonStr = await rootBundle.loadString('assets/json/courses.json');
     return json.decode(jsonStr);
